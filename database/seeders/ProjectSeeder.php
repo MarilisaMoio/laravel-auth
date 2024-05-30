537ed363e2861debdca0575007a9532c9b9cf096
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-30 16:41:36 +0200

    feat: popolate table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,15 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 8; $i++){
+            $newProject = new Project();
+            $newProject->name = $faker->words(2, true) . ' Project';
+            $newProject->slug = Str::slug($newProject->name, '-');
+            $newProject->client_name = $faker->name();
+            $newProject->summary = $faker->paragraphs(2, true);
+            $newProject->save();
+        };
     }
 }
